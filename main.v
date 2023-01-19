8b190d4ea6b52f22fd078636e70f0f195c53604f
Nikita Goncarenko <69197950+nikeedev@users.noreply.github.com>
2023-01-19 23:27:56 +0100

    Update main.v

--- main.v
+++ main.v
@@ -1,1 +1,22 @@
+module main
 
+import net.http
+import os
+  
+const api_key = os.getenv("API_KEY")
+const api_url = "https://api.openai.com/v1/engines/davinci-codex/completions"
+
+fn main() {
+    headers := {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + api_key
+    }
+    body := {
+        "prompt": "What is the capital of France?",
+        "temperature": 0.5
+    }
+    json_body := json.encode(body)
+    res := http.post(api_url, headers=headers, body=json_body)
+    println(res.body.str())
+}
+  
